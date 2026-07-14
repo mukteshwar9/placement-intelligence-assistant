7d9ada9acd08fe40032afe81a4059ac28b1a6109
Mani Mukteshwar <mukteshwar@example.com>
2026-07-14 23:32:15 +0530

    Fix deployment dependencies

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-
-DATA_PATH = "data"
-VECTOR_DB_PATH = "vectorstore"
+if "GROQ_API_KEY" in st.secrets:
+    GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
+else:
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
